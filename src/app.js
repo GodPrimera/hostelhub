9f5b69bd252744da3530eacab22df9ff4042b7cb
GodPrimera <afatabeh07@gmail.com>
2026-09-19 08:14:10 +0100

    configured project for render

--- src/app.js
+++ src/app.js
@@ -5,8 +5,10 @@ const flash = require('connect-flash')
 const authRoutes = require('./routes/authRoutes')
 const dashboardRoutes = require('./routes/dashboardRoutes')
 const checkAuthenticated = require('./middlewares/checkAuthenticated');
-
+const path = require('path');
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 if (process.env.NODE_ENV === 'production') app.set('trust proxy', 1);
 
 app.use(express.urlencoded({ extended:true }));
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dns').setServers(['8.8.8.8', '1.1.1.1']);
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const app = require('./app');
